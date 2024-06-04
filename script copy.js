a98b1f6b55ea506e06faccb50c2ba06afb47edd8
unknown <el_word9@yahoo.com>
2024-06-04 14:38:22 -0500

    updates

--- script copy.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function() {
-  var navbar = document.getElementById("navbar");
-  if (window.pageYOffset > 2) {
-      navbar.classList.add("shrink");
-  } else {
-      navbar.classList.remove("shrink");
-  }
-});
-
